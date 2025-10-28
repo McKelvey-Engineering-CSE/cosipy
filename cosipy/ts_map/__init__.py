0908264e79a7f734d289b257995d0d26c211b98d
jdbuhler <jbuhler@wustl.edu>
2025-10-28 11:51:33 -0500

    make sure code has access to faster GRB classes

--- cosipy/ts_map/__init__.py
+++ cosipy/ts_map/__init__.py
@@ -1,3 +1,5 @@
 from .fast_ts_fit import FastTSMap
 from .fast_norm_fit import FastNormFit
+from .faster_ts_fit import FasterTSMap
+from .faster_norm_fit import FasterNormFit
 from .moc_ts_fit import MOCTSMap
